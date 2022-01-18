b7652004c9bce816ad383c0705d04fef6f375105
Matthew Casperson <matthewcasperson@gmail.com>
2022-01-18 15:15:00 +1000

    Fixed some layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import {ReactMediaRecorder} from "react-media-recorder";
 import Button from '@mui/material/Button';
-import {Grid} from "@mui/material";
+import {FormControl, Grid, InputLabel, MenuItem, Select} from "@mui/material";
+import {useState} from "react";
 
 const readyToRecordStates = ["stopped", "idle"];
 const recordingStates = ["recording"];
 
 function App() {
+    const [inputLanguage, setInputLanguage] = useState("English");
+    const [translatedLanguage, setTranslatedLanguage] = useState("German");
+
+    const handleInputLanguageChange = (event) => {
+        setInputLanguage(event.target.value);
+    };
+
+    const handleTranslatedLanguageChange = (event) => {
+        setTranslatedLanguage(event.target.value);
+    };
+
+
     return (
         <div className="App">
             <h1>Universal Translator</h1>
@@ -16,16 +29,59 @@ function App() {
                     <Grid
                         container
                         spacing={2}
+                        rowGap={2}
                         justifyContent="center"
                         alignItems="center">
                         <Grid item md={3} xs={0}/>
+                        <Grid md={6} xs={12}>
+                            <FormControl fullWidth>
+                                <InputLabel id="language-label">Recorded Language</InputLabel>
+                                <Select
+                                    labelId="language-label"
+                                    value={inputLanguage}
+                                    label="Recorded Language"
+                                    onChange={handleInputLanguageChange}
+                                >
+                                    <MenuItem value={"English"}>English</MenuItem>
+                                    <MenuItem value={"German"}>German</MenuItem>
+                                    <MenuItem value={"Japanese"}>Japanese</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item md={3} xs={0}/>
+                        <Grid item md={3} xs={0}/>
+                        <Grid md={6} xs={12}>
+                            <FormControl fullWidth>
+                                <InputLabel id="language-label">Translated Language</InputLabel>
+                                <Select
+                                    labelId="language-label"
+                                    value={translatedLanguage}
+                                    label="Translated Language"
+                                    onChange={handleTranslatedLanguageChange}
+                                >
+                                    <MenuItem value={"English"}>English</MenuItem>
+                                    <MenuItem value={"German"}>German</MenuItem>
+                                    <MenuItem value={"Japanese"}>Japanese</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                        <Grid item md={3} xs={0}/>
+                        <Grid item md={3} xs={0}/>
                         <Grid item md={3} xs={12}>
-                            <Button variant="contained" onClick={startRecording} disabled={readyToRecordStates.indexOf(status) === -1}>
+                            <Button
+                                variant="contained"
+                                className={"fullWidth"}
+                                onClick={startRecording}
+                                disabled={readyToRecordStates.indexOf(status) === -1}>
                                 Start Recording
                             </Button>
                         </Grid>
                         <Grid item md={3} xs={12}>
-                            <Button variant="contained" onClick={stopRecording} disabled={recordingStates.indexOf(status) === -1}>
+                            <Button
+                                variant="contained"
+                                className={"fullWidth"}
+                                onClick={stopRecording}
+                                disabled={recordingStates.indexOf(status) === -1}>
                                 Stop Recording
                             </Button>
                         </Grid>
